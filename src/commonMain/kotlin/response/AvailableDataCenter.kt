42a948664ab24acb5309aa1e36ad8df1d765e565
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-07 23:42:14 +0000

    Improve some KDoc's

--- src/commonMain/kotlin/response/AvailableDataCenter.kt
+++ src/commonMain/kotlin/response/AvailableDataCenter.kt
@@ -5,6 +5,9 @@ import kotlinx.serialization.Serializable
 
 /**
  * A data center supported by the Universalis API
+ * @property name Name of a data center supported by the Universalis API
+ * @property region Region of a data center supported by the Universalis API
+ * @property worlds World IDs of a data center supported by the Universalis API
  */
 @JsExport @Serializable class AvailableDataCenter(
     val name: String,
--- src/commonMain/kotlin/response/AvailableWorld.kt
+++ src/commonMain/kotlin/response/AvailableWorld.kt
@@ -5,5 +5,7 @@ import kotlinx.serialization.Serializable
 
 /**
  * ID and name of a world supported by the Universalis API
+ * @property id ID of a world supported by the Universalis API
+ * @property name Name of a world supported by the Universalis API
  */
 @JsExport @Serializable class AvailableWorld(val id: Short, val name: String)
