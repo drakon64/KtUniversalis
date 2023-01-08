a514dd2f1a41b270d25722e145e46972bbd8fea6
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 15:05:51 +0000

    Remove confusing KDoc for `MostRecentlyUpdatedItems`

--- src/commonMain/kotlin/entities/MostRecentlyUpdatedItems.kt
+++ src/commonMain/kotlin/entities/MostRecentlyUpdatedItems.kt
@@ -3,8 +3,5 @@ package cloud.drakon.ktuniversalis.entities
 import kotlin.js.JsExport
 import kotlinx.serialization.Serializable
 
-/**
- * The least-recently updated items on the specified world or data center, along with the upload times for each item
- */
 @JsExport @Serializable
 class MostRecentlyUpdatedItems(val items: Array<WorldItemRecency>? = null)
