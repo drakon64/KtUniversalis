4e8e5f13cf5e1d0529e4935b2f7e45b2e54b881a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 12:39:47 +0000

    Add KDocs for `MarketBoardCurrentData`

--- src/commonMain/kotlin/response/MarketBoardCurrentData.kt
+++ src/commonMain/kotlin/response/MarketBoardCurrentData.kt
@@ -4,6 +4,34 @@ import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * @property itemId The item ID
+ * @property worldId The world ID, if applicable
+ * @property lastUploadTime The last upload time for this endpoint, in milliseconds since the UNIX epoch
+ * @property listings The currently-shown listings
+ * @property recentHistory The currently-shown sales
+ * @property dcName The DC name, if applicable
+ * @property regionName The region name, if applicable
+ * @property currentAveragePrice The average listing price, with outliers removed beyond 3 standard deviations of the mean
+ * @property currentAveragePriceNQ The average NQ listing price, with outliers removed beyond 3 standard deviations of the mean
+ * @property currentAveragePriceHQ The average HQ listing price, with outliers removed beyond 3 standard deviations of the mean
+ * @property regularSaleVelocity The average number of sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
+ * @property nqSaleVelocity The average number of NQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
+ * @property hqSaleVelocity The average number of HQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
+ * @property averagePrice The average sale price, with outliers removed beyond 3 standard deviations of the mean
+ * @property averagePriceNQ The average NQ sale price, with outliers removed beyond 3 standard deviations of the mean
+ * @property averagePriceHQ The average HQ sale price, with outliers removed beyond 3 standard deviations of the mean
+ * @property minPrice The minimum listing price
+ * @property minPriceNQ The minimum NQ listing price
+ * @property minPriceHQ The minimum HQ listing price
+ * @property maxPrice The maximum listing price
+ * @property maxPriceNQ The maximum NQ listing price
+ * @property maxPriceHQ The maximum HQ listing price
+ * @property stackSizeHistogram A map of quantities to listing counts, representing the number of listings of each quantity
+ * @property stackSizeHistogramNQ A map of quantities to NQ listing counts, representing the number of listings of each quantity
+ * @property stackSizeHistogramHQ A map of quantities to HQ listing counts, representing the number of listings of each quantity
+ * @property worldName The world name, if applicable
+ */
 @JsExport @Serializable class MarketBoardCurrentData(
     @SerialName("itemID") val itemId: Int,
     @SerialName("worldID") val worldId: Int? = null,
