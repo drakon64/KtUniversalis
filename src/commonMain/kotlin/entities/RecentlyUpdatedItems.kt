599074386b0d52ca01c73faf1385b449feefb7dc
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 16:08:52 +0000

    Add KDoc's for `RecentlyUpdatedItems` and `WorldUploadCount`

--- src/commonMain/kotlin/entities/RecentlyUpdatedItems.kt
+++ src/commonMain/kotlin/entities/RecentlyUpdatedItems.kt
@@ -3,5 +3,8 @@ package cloud.drakon.ktuniversalis.entities
 import kotlin.js.JsExport
 import kotlinx.serialization.Serializable
 
+/**
+ * @property items An array of item upload information
+ */
 @JsExport @Serializable
 class RecentlyUpdatedItems(val items: Array<WorldItemRecency>? = null)
--- src/commonMain/kotlin/entities/WorldUploadCount.kt
+++ src/commonMain/kotlin/entities/WorldUploadCount.kt
@@ -3,4 +3,8 @@ package cloud.drakon.ktuniversalis.entities
 import kotlin.js.JsExport
 import kotlinx.serialization.Serializable
 
+/**
+ * @property count The number of times an upload has occurred on this world
+ * @property proportion The proportion of uploads on this world to the total number of uploads
+ */
 @JsExport @Serializable class WorldUploadCount(val count: Int, val proportion: Double)
