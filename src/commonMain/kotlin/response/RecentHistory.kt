f7206e4e1436132b68ba7f14ddc8ec79f8ca1846
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 14:07:09 +0000

    Add KDoc's and missing fields for `RecentHistory`

--- src/commonMain/kotlin/response/RecentHistory.kt
+++ src/commonMain/kotlin/response/RecentHistory.kt
@@ -1,14 +1,28 @@
 package cloud.drakon.ktuniversalis.response
 
 import kotlin.js.JsExport
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * @property hq Whether or not the item was high-quality
+ * @property pricePerUnit The price per unit sold
+ * @property quantity The stack size sold
+ * @property timestamp The sale time, in seconds since the UNIX epoch
+ * @property onMannequin Whether or not this was purchased from a mannequin
+ * @property worldName The world name, if applicable
+ * @property worldId The world ID, if applicable
+ * @property buyerName The buyer name
+ * @property total The total price
+ */
 @JsExport @Serializable class RecentHistory(
     val hq: Boolean,
     val pricePerUnit: Int,
     val quantity: Int,
     val timestamp: Long,
-    val onMannequin: Boolean,
-    val buyerName: String,
+    val onMannequin: Boolean? = null,
+    val worldName: String? = null,
+    @SerialName("worldID") val worldId: Int? = null,
+    val buyerName: String? = null,
     val total: Int,
 )
