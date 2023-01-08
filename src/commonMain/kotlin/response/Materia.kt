7267947a5bf7a73eb0352eef40ace282586adf28
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 14:01:15 +0000

    Add KDoc's for `Materia`

--- src/commonMain/kotlin/response/Materia.kt
+++ src/commonMain/kotlin/response/Materia.kt
@@ -4,6 +4,10 @@ import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * @property slotId The materia slot
+ * @property materiaId The materia item ID
+ */
 @JsExport @Serializable class Materia(
     @SerialName("slotID") val slotId: Int,
     @SerialName("materiaID") val materiaId: Int,
