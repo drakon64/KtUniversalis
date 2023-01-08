80318d4e8f54f8371ebe1f1e794d0f8dcd592b50
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-01-08 14:09:05 +0000

    Add KDoc's and missing fields for `RecentlyUpdatedItem`

--- src/commonMain/kotlin/response/RecentlyUpdatedItem.kt
+++ src/commonMain/kotlin/response/RecentlyUpdatedItem.kt
@@ -4,9 +4,15 @@ import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * @property itemId The item ID
+ * @property lastUploadTime The last upload time for the item on the listed world
+ * @property worldId The world ID
+ * @property worldName The world name
+ */
 @JsExport @Serializable class RecentlyUpdatedItem(
     @SerialName("itemID") val itemId: Int,
     val lastUploadTime: Long,
     @SerialName("worldID") val worldId: Int,
-    val worldName: String,
+    val worldName: String? = null,
 )
