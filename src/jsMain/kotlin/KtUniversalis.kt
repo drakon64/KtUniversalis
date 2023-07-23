84aa8cfe232beb1d48ceb096e1cf360d059c6290
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-23 22:00:43 +0100

    Rename Array to List in KDoc

--- src/jsMain/kotlin/KtUniversalis.kt
+++ src/jsMain/kotlin/KtUniversalis.kt
@@ -152,16 +152,16 @@ import kotlinx.coroutines.promise
     }
 
     /**
-     * Returns the data currently shown on the market board for the requested array of item IDs and world or data center.
+     * Returns the data currently shown on the market board for the requested list of item IDs and world or data center.
      * @param worldDcRegion The world, data center, or region to retrieve data for. This may be an ID or a name. Regions should be specified as Japan, Europe, North-America, Oceania, China, or 中国.
-     * @param itemIds Array of item IDs to retrieve data for.
+     * @param itemIds List of item IDs to retrieve data for.
      * @param listings The number of listings to return. By default, all listings will be returned.
      * @param entries The number of recent history entries to return. By default, a maximum of `5` entries will be returned.
      * @param noGst If the result should not have Gil sales tax (GST) factored in. GST is applied to all consumer purchases in-game, and is separate from the retainer city tax that impacts what sellers receive. By default, GST is factored in.
      * @param hq Filter for HQ listings and entries. By default, both HQ and NQ listings and entries will be returned.
      * @param statsWithin The amount of time before now to calculate stats over, in milliseconds. By default, this is 7 days.
      * @param entriesWithin The amount of time before now to take entries within, in seconds. Negative values will be ignored.
-     * @param fields An array of fields that should be included in the response, if omitted will return all fields. For example if you're only interested in the listings price per unit you can set this to `listings.pricePerUnit`.
+     * @param fields An list of fields that should be included in the response, if omitted will return all fields. For example if you're only interested in the listings price per unit you can set this to `listings.pricePerUnit`.
      * @throws ItemIdsException At least one and less than or equal to 100 item IDs are required.
      * @throws InvalidParameterException The parameters are invalid.
      * @throws InvalidWorldDcItemException The world/DC or item requested is invalid.
@@ -258,9 +258,9 @@ import kotlinx.coroutines.promise
     }
 
     /**
-     * Returns the history data for the requested array of item IDs and world or data center.
+     * Returns the history data for the requested list of item IDs and world or data center.
      * @param worldDcRegion The world or data center to retrieve data for. This may be an ID or a name. Regions should be specified as Japan, Europe, North-America, Oceania, China, or 中国.
-     * @param itemIds Array of item IDs to retrieve data for.
+     * @param itemIds List of item IDs to retrieve data for.
      * @param entriesToReturn The number of entries to return. By default, this is set to `1800`, but may be set to a maximum of `999999`.
      * @param statsWithin The amount of time before now to calculate stats over, in milliseconds. By default, this is `7` days.
      * @param entriesWithin The amount of time before now to take entries within, in seconds. Negative values will be ignored.
@@ -358,7 +358,7 @@ import kotlinx.coroutines.promise
     }
 
     /**
-     * Returns an array of marketable item IDs.
+     * Returns a list of marketable item IDs.
      * @throws UniversalisException The Universalis API returned an unexpected return code.
      */
     @JsExport.Ignore actual suspend fun getMarketableItems(): List<Int> =
