9accb9a16e091c230139614c9579360b54811911
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-07-26 21:42:59 +0100

    Make `World.name` non-nullable The API docs says this is nullable but it doesn't actually return null

--- src/commonMain/kotlin/entities/World.kt
+++ src/commonMain/kotlin/entities/World.kt
@@ -8,4 +8,4 @@ import kotlinx.serialization.Serializable
  * @property id ID of a world supported by the Universalis API
  * @property name Name of a world supported by the Universalis API
  */
-@JsExport @Serializable data class World(val id: Int, val name: String? = null)
+@JsExport @Serializable data class World(val id: Int, val name: String)
