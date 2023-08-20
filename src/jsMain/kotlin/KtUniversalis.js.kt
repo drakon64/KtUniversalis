2b6366d47b59ce3889f5dfff1ddcd08e4ec560a7
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-08-20 19:00:24 +0100

    Re-add missing KDoc's

--- src/jsMain/kotlin/KtUniversalis.js.kt
+++ src/jsMain/kotlin/KtUniversalis.js.kt
@@ -2,6 +2,13 @@
 
 package cloud.drakon.ktuniversalis
 
+import cloud.drakon.ktuniversalis.exception.InvalidEntriesException
+import cloud.drakon.ktuniversalis.exception.InvalidParameterException
+import cloud.drakon.ktuniversalis.exception.InvalidWorldDcException
+import cloud.drakon.ktuniversalis.exception.InvalidWorldDcItemException
+import cloud.drakon.ktuniversalis.exception.InvalidWorldException
+import cloud.drakon.ktuniversalis.exception.ItemIdsException
+import cloud.drakon.ktuniversalis.exception.UniversalisException
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.js.Js
 import io.ktor.client.plugins.DefaultRequest
@@ -21,14 +28,31 @@ internal actual val ktorClient = HttpClient(Js) {
     }
 }
 
+/**
+ * Returns all data centers supported by the Universalis API. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getAvailableDataCentersAsync() = GlobalScope.promise {
     getAvailableDataCenters().toTypedArray()
 }
 
+/**
+ * Returns the IDs and names of all worlds supported by the Universalis API. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getAvailableWorldsAsync() = GlobalScope.promise {
     getAvailableWorlds().toTypedArray()
 }
 
+/**
+ * Returns the least-recently updated items on the specified world or data center, along with the upload times for each item. For use outside of Kotlin coroutines.
+ * @param world The world to request data for.
+ * @param dcName The data center to request data for.
+ * @param entries The number of entries to return (default `50`, max `200`).
+ * @throws InvalidWorldDcException The world/DC requested is invalid.
+ * @throws InvalidEntriesException `entries` must be between `0` and `200`.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getLeastRecentlyUpdatedItemsAsync(
     world: String? = null,
     dcName: String? = null,
@@ -37,6 +61,22 @@ internal actual val ktorClient = HttpClient(Js) {
     getLeastRecentlyUpdatedItems(world, dcName, entries)
 }
 
+/**
+ * Returns the data currently shown on the market board for the requested list of item IDs and world or data center. For use outside of Kotlin coroutines.
+ * @param worldDcRegion The world, data center, or region to retrieve data for. This may be an ID or a name. Regions should be specified as Japan, Europe, North-America, Oceania, China, or 中国.
+ * @param itemIds List of item IDs to retrieve data for.
+ * @param listings The number of listings to return. By default, all listings will be returned.
+ * @param entries The number of recent history entries to return. By default, a maximum of `5` entries will be returned.
+ * @param noGst If the result should not have Gil sales tax (GST) factored in. GST is applied to all consumer purchases in-game, and is separate from the retainer city tax that impacts what sellers receive. By default, GST is factored in.
+ * @param hq Filter for HQ listings and entries. By default, both HQ and NQ listings and entries will be returned.
+ * @param statsWithin The amount of time before now to calculate stats over, in milliseconds. By default, this is 7 days.
+ * @param entriesWithin The amount of time before now to take entries within, in seconds. Negative values will be ignored.
+ * @param fields An list of fields that should be included in the response, if omitted will return all fields. For example if you're only interested in the listings price per unit you can set this to `listings.pricePerUnit`.
+ * @throws ItemIdsException At least one and less than or equal to 100 item IDs are required.
+ * @throws InvalidParameterException The parameters are invalid.
+ * @throws InvalidWorldDcItemException The world/DC or item requested is invalid.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getMarketBoardCurrentDataAsync(
     worldDcRegion: String,
     itemIds: Array<Int>,
@@ -61,6 +101,17 @@ internal actual val ktorClient = HttpClient(Js) {
     )
 }
 
+/**
+ * Returns the history data for the requested list of item IDs and world or data center. For use outside of Kotlin coroutines.
+ * @param worldDcRegion The world or data center to retrieve data for. This may be an ID or a name. Regions should be specified as Japan, Europe, North-America, Oceania, China, or 中国.
+ * @param itemIds List of item IDs to retrieve data for.
+ * @param entriesToReturn The number of entries to return. By default, this is set to `1800`, but may be set to a maximum of `999999`.
+ * @param statsWithin The amount of time before now to calculate stats over, in milliseconds. By default, this is `7` days.
+ * @param entriesWithin The amount of time before now to take entries within, in seconds. Negative values will be ignored.
+ * @throws ItemIdsException At least one and less than or equal to 100 item IDs are required.
+ * @throws InvalidWorldDcItemException The world/DC or item requested is invalid.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getMarketBoardSaleHistoryAsync(
     worldDcRegion: String,
     itemIds: Array<Int>,
@@ -73,14 +124,33 @@ internal actual val ktorClient = HttpClient(Js) {
     )
 }
 
+/**
+ * Returns the current tax rate data for the specified world. For use outside of Kotlin coroutines.
+ * @param world The world or to retrieve data for. This may be an ID or a name.
+ * @throws InvalidWorldException The world requested is invalid.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getMarketTaxRatesAsync(world: String) = GlobalScope.promise {
     getMarketTaxRates(world)
 }
 
+/**
+ * Returns a list of marketable item IDs. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getMarketableItemsAsync() = GlobalScope.promise {
     getMarketableItems().toIntArray()
 }
 
+/**
+ * Returns the most-recently updated items on the specified world or data center, along with the upload times for each item. For use outside of Kotlin coroutines.
+ * @param world The world to request data for.
+ * @param dcName The data center to request data for.
+ * @param entries The number of entries to return (default `50`, max `200`).
+ * @throws InvalidWorldDcException The world/DC requested is invalid.
+ * @throws InvalidEntriesException `entries` must be between `0` and `200`.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getMostRecentlyUpdatedItemsAsync(
     world: String? = null,
     dcName: String? = null,
@@ -89,14 +159,26 @@ internal actual val ktorClient = HttpClient(Js) {
     getMostRecentlyUpdatedItems(world, dcName, entries)
 }
 
+/**
+ * Returns the total upload counts for each client application that uploads data to Universalis. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getUploadCountsByUploadApplicationAsync() = GlobalScope.promise {
     getUploadCountsByUploadApplication().toTypedArray()
 }
 
+/**
+ * Returns the world upload counts and proportions of the total uploads for each world. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getUploadCountsByWorldAsync() = GlobalScope.promise {
     getUploadCountsByWorld()
 }
 
+/**
+ * Returns the number of uploads per day over the past 30 days. For use outside of Kotlin coroutines.
+ * @throws UniversalisException The Universalis API returned an unexpected return code.
+ */
 @JsExport fun getUploadsPerDayAsync() = GlobalScope.promise {
     getUploadsPerDay()
 }
