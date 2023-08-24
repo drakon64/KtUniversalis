3ee32f62b807cc8b4be6d9334183dfb1bb71f0d9
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-08-24 23:16:25 +0100

    Reduce compiler warnings

--- src/commonMain/kotlin/entities/Sale.kt
+++ src/commonMain/kotlin/entities/Sale.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalJsExport::class)
+
 package cloud.drakon.ktuniversalis.entities
 
+import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
