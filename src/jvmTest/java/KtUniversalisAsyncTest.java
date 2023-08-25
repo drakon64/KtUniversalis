b912ae3007c89759fcbc32f92000f7b3a63e9e6b
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-08-25 22:23:34 +0100

    Remove static imports from Java tests

--- src/jvmTest/java/KtUniversalisAsyncTest.java
+++ src/jvmTest/java/KtUniversalisAsyncTest.java
@@ -1,61 +1,83 @@
+import cloud.drakon.ktuniversalis.KtUniversalis;
 import org.junit.jupiter.api.Test;
 
-import static cloud.drakon.ktuniversalis.KtUniversalis.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class KtUniversalisAsyncTest {
     @Test
     void getAvailableDataCentersTest() {
-        assertDoesNotThrow(() -> System.out.println(getAvailableDataCentersAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getAvailableDataCentersAsync().get())
+        );
     }
 
     @Test
     void getAvailableWorldsTest() {
-        assertDoesNotThrow(() -> System.out.println(getAvailableWorldsAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getAvailableWorldsAsync().get())
+        );
     }
 
     @Test
     void getLeastRecentlyUpdatedItemsTest() {
-        assertDoesNotThrow(() -> System.out.println(getLeastRecentlyUpdatedItemsAsync("Cerberus").get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getLeastRecentlyUpdatedItemsAsync("Cerberus").get())
+        );
     }
 
     @Test
     void getMarketBoardCurrentDataTest() {
-        assertDoesNotThrow(() -> System.out.println(getMarketBoardCurrentDataAsync("Europe", 38264).get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getMarketBoardCurrentDataAsync("Europe", 38264).get())
+        );
     }
 
     @Test
     void getMarketBoardSaleHistoryTest() {
-        assertDoesNotThrow(() -> System.out.println(getMarketBoardSaleHistoryAsync("Europe", 38264).get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getMarketBoardSaleHistoryAsync("Europe", 38264).get())
+        );
     }
 
     @Test
     void getMarketTaxRatesTest() {
-        assertDoesNotThrow(() -> System.out.println(getMarketTaxRatesAsync("Cerberus").get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getMarketTaxRatesAsync("Cerberus").get())
+        );
     }
 
     @Test
     void getMarketableItemsTest() {
-        assertDoesNotThrow(() -> System.out.println(getMarketableItemsAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getMarketableItemsAsync().get())
+        );
     }
 
     @Test
     void getMostRecentlyUpdatedItemsTest() {
-        assertDoesNotThrow(() -> System.out.println(getMostRecentlyUpdatedItemsAsync("Cerberus").get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getMostRecentlyUpdatedItemsAsync("Cerberus").get())
+        );
     }
 
     @Test
     void getUploadCountsByUploadApplicationTest() {
-        assertDoesNotThrow(() -> System.out.println(getUploadCountsByUploadApplicationAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getUploadCountsByUploadApplicationAsync().get())
+        );
     }
 
     @Test
     void getUploadCountsByWorldTest() {
-        assertDoesNotThrow(() -> System.out.println(getUploadCountsByWorldAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getUploadCountsByWorldAsync().get())
+        );
     }
 
     @Test
     void getUploadsPerDayTest() {
-        assertDoesNotThrow(() -> System.out.println(getUploadsPerDayAsync().get()));
+        assertDoesNotThrow(() -> System.out.println(
+                KtUniversalis.getUploadsPerDayAsync().get())
+        );
     }
 }
