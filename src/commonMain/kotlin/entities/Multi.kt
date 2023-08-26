4ceda198da7ca26001bad199237751018766e7cb
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-08-26 16:52:41 +0100

    Add missing KDoc for `Multi`

--- src/commonMain/kotlin/entities/Multi.kt
+++ src/commonMain/kotlin/entities/Multi.kt
@@ -7,6 +7,15 @@ import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * @property itemIds The item IDs that were requested.
+ * @property items The item data that was requested, keyed on the item ID.
+ * @property worldId The ID of the world requested, if applicable.
+ * @property dcName The name of the DC requested, if applicable.
+ * @property regionName The name of the region requested, if applicable.
+ * @property unresolvedItems A list of IDs that could not be resolved to any item data.
+ * @property worldName The name of the world requested, if applicable.
+ */
 @JsExport @Serializable data class Multi<T: MarketBoard>(
     @SerialName("itemIDs") val itemIds: Set<Int>? = null,
     val items: Map<Int, T>? = null,
