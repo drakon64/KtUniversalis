a4ce30d34cad47ba0cccd639696079604b16654a
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-08-30 19:54:27 +0100

    Adjust KDoc for `getRecentlyUpdatedItems`

--- src/commonMain/kotlin/GetRecentlyUpdatedItems.kt
+++ src/commonMain/kotlin/GetRecentlyUpdatedItems.kt
@@ -7,12 +7,6 @@ import cloud.drakon.ktuniversalis.world.World
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
-/**
- * @param world The world to request data for.
- * @param dcName The data center to request data for.
- * @param entries The number of entries to return (default `50`, max `200`).
- * @throws UniversalisException The Universalis API returned an unexpected return code.
- */
 private suspend fun getRecentlyUpdatedItems(
     world: World? = null,
     dcName: DataCenter? = null,
--- src/jsMain/kotlin/GetRecentlyUpdatedItems.js.kt
+++ src/jsMain/kotlin/GetRecentlyUpdatedItems.js.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.promise
  * @param entries The number of entries to return (default `50`, max `200`).
  * @throws UniversalisException The Universalis API returned an unexpected return code.
  */
-@JsExport @JsName("getLeastRecentlyUpdatedItemsByWorldAsync")
+@JsExport @JsName("getLeastRecentlyUpdatedItemsByWorld")
 fun getLeastRecentlyUpdatedItemsAsync(
     world: World,
     entries: Short? = null,
@@ -24,12 +24,12 @@ fun getLeastRecentlyUpdatedItemsAsync(
 }
 
 /**
- * Returns the least-recently updated items on the specified world or data center, along with the upload times for each item.
+ * Returns the least-recently updated items on the specified data center, along with the upload times for each item.
  * @param dcName The data center to request data for.
  * @param entries The number of entries to return (default `50`, max `200`).
  * @throws UniversalisException The Universalis API returned an unexpected return code.
  */
-@JsExport @JsName("getLeastRecentlyUpdatedItemsByDataCenterAsync")
+@JsExport @JsName("getLeastRecentlyUpdatedItemsByDataCenter")
 fun getLeastRecentlyUpdatedItemsAsync(
     dcName: DataCenter,
     entries: Short? = null,
@@ -43,7 +43,7 @@ fun getLeastRecentlyUpdatedItemsAsync(
  * @param entries The number of entries to return (default `50`, max `200`).
  * @throws UniversalisException The Universalis API returned an unexpected return code.
  */
-@JsExport @JsName("getMostRecentlyUpdatedItemsByWorldAsync")
+@JsExport @JsName("getMostRecentlyUpdatedItemsByWorld")
 fun getMostRecentlyUpdatedItemsAsync(
     world: World,
     entries: Short? = null,
@@ -57,7 +57,7 @@ fun getMostRecentlyUpdatedItemsAsync(
  * @param entries The number of entries to return (default `50`, max `200`).
  * @throws UniversalisException The Universalis API returned an unexpected return code.
  */
-@JsExport @JsName("getMostRecentlyUpdatedItemsByDataCenterAsync")
+@JsExport @JsName("getMostRecentlyUpdatedItemsByDataCenter")
 fun getMostRecentlyUpdatedItemsAsync(
     dcName: DataCenter,
     entries: Short? = null,
