fe42fd911b0ba9d7616f0ec6531cfd3217e0dcbe
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-01 18:16:31 +0100

    Remove serialiser from `MarketBoard`

--- src/commonMain/kotlin/entities/MarketBoard.kt
+++ src/commonMain/kotlin/entities/MarketBoard.kt
@@ -4,6 +4,5 @@ package cloud.drakon.ktuniversalis.entities
 
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
-import kotlinx.serialization.Serializable
 
-@JsExport @Serializable sealed interface MarketBoard
+@JsExport sealed interface MarketBoard
