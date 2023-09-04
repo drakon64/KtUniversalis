e32910d636f66b7d37617906035e052ea54a30ff
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-04 20:29:27 +0100

    Remove redundant class properties

--- src/commonMain/kotlin/entities/History.kt
+++ src/commonMain/kotlin/entities/History.kt
@@ -2,35 +2,24 @@
 
 package cloud.drakon.ktuniversalis.entities
 
-import cloud.drakon.ktuniversalis.world.DataCenter
-import cloud.drakon.ktuniversalis.world.World
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * @property itemId The item ID
- * @property worldId The world ID, if applicable
  * @property lastUploadTime The last upload time for this endpoint, in milliseconds since the UNIX epoch
  * @property entries The historical sales
- * @property dcName The DC name, if applicable
- * @property regionName The region name, if applicable
  * @property stackSizeHistogram A map of quantities to sale counts, representing the number of sales of each quantity
  * @property stackSizeHistogramNq A map of quantities to NQ sale counts, representing the number of sales of each quantity
  * @property stackSizeHistogramHq A map of quantities to HQ sale counts, representing the number of sales of each quantity
  * @property regularSaleVelocity The average number of sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
  * @property nqSaleVelocity The average number of NQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
  * @property hqSaleVelocity The average number of HQ sales per day, over the past seven days (or the entirety of the shown sales, whichever comes first)
- * @property worldName The world name, if applicable
  */
 @JsExport @Serializable data class History(
-    @SerialName("itemID") val itemId: Int,
-    @SerialName("worldID") val worldId: Short? = null,
     val lastUploadTime: Long,
     val entries: List<MinimizedSale>? = null,
-    val dcName: DataCenter? = null,
-    val regionName: String? = null,
     val stackSizeHistogram: StackSizeHistogram = null,
 
     @SerialName("stackSizeHistogramNQ")
@@ -42,5 +31,4 @@ import kotlinx.serialization.Serializable
     val regularSaleVelocity: Double,
     val nqSaleVelocity: Double,
     val hqSaleVelocity: Double,
-    val worldName: World? = null,
 ): MarketBoard
