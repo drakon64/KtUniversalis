99a313a86062b9122f2a601482189a424468b5d7
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-16 11:52:00 +0100

    Remove properties that could be inferred from the calling function parameters

--- src/commonMain/kotlin/entities/Multi.kt
+++ src/commonMain/kotlin/entities/Multi.kt
@@ -2,8 +2,6 @@
 
 package cloud.drakon.ktuniversalis.entities
 
-import cloud.drakon.ktuniversalis.world.DataCenter
-import cloud.drakon.ktuniversalis.world.World
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.js.ExperimentalJsExport
@@ -12,17 +10,11 @@ import kotlin.js.JsExport
 /**
  * @property itemIds The item IDs that were requested.
  * @property items The item data that was requested, keyed on the item ID.
- * @property dataCenter The [DataCenter] requested, if applicable.
- * @property regionName The name of the region requested, if applicable.
  * @property unresolvedItems A list of IDs that could not be resolved to any item data.
- * @property world The [World] requested, if applicable.
  */
 @JsExport @Serializable
 data class Multi<T : MarketBoard>(
     @SerialName("itemIDs") val itemIds: List<Int>? = null,
     val items: Map<Int, T>? = null,
-    @SerialName("dcName") val dataCenter: DataCenter? = null,
-    val regionName: String? = null,
     val unresolvedItems: List<Int>? = null,
-    @SerialName("worldName") val world: World? = null,
 )
