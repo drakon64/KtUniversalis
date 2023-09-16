90f096b58f2d57df7208f08e807b8820d283d232
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-16 23:59:27 +0100

    Remove `World` enum from `WorldUploadCount`

--- src/commonMain/kotlin/entities/WorldUploadCount.kt
+++ src/commonMain/kotlin/entities/WorldUploadCount.kt
@@ -2,14 +2,13 @@
 
 package cloud.drakon.ktuniversalis.entities
 
-import cloud.drakon.ktuniversalis.world.World
 import kotlinx.serialization.Serializable
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 
 /**
- * @property count The number of times an upload has occurred on this [World].
- * @property proportion The proportion of uploads on this [World] to the total number of uploads.
+ * @property count The number of times an upload has occurred on this world.
+ * @property proportion The proportion of uploads on this world to the total number of uploads.
  */
 @JsExport @Serializable
 data class WorldUploadCount(val count: Int, val proportion: Double)
