af3b5628e388e051e697bfd6dfa46488b23b2ecf
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-16 15:28:42 +0100

    Remove unused functions from `ProblemDetails`

--- src/commonMain/kotlin/ThrowException.kt
+++ src/commonMain/kotlin/ThrowException.kt
@@ -7,13 +7,15 @@ import io.ktor.client.statement.HttpResponse
 import kotlinx.serialization.Serializable
 
 @Serializable
-private data class ProblemDetails(
+private class ProblemDetails(
     val type: String? = null,
     val title: String? = null,
     val status: Short? = null,
     val detail: String? = null,
     val instance: String? = null,
-)
+) {
+    override fun toString() = "ProblemDetails(type=$type, title=$title, status=$status, detail=$detail, instance=$instance)"
+}
 
 private suspend fun getExceptionMessage(httpResponse: HttpResponse) = (httpResponse.body() as ProblemDetails).toString()
 
