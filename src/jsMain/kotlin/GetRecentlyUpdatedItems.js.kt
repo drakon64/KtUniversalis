07b20a675a83c1280042788c7aa64e0fc6937445
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-23 23:33:27 +0100

    More KDoc fixes

--- src/jsMain/kotlin/GetRecentlyUpdatedItems.js.kt
+++ src/jsMain/kotlin/GetRecentlyUpdatedItems.js.kt
@@ -45,7 +45,7 @@ fun getLeastRecentlyUpdatedItemsAsync(
  * Returns the most-recently updated items on the specified world, along with the upload times for each item.
  *
  * This function is designed to be used from JavaScript. For use within Kotlin, see [getMostRecentlyUpdatedItems].
- * @param world The world to request data for.
+ * @param world The [World] to request data for.
  * @param entries The number of entries to return (default `50`, max `200`).
  * @throws UniversalisException The Universalis API returned an unexpected return code.
  */
