137df1778e8a3056e4d56860339ff556f8edf72c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-09-24 15:42:44 +0100

    `MarketBoard` should be serializable

--- src/commonMain/kotlin/entities/MarketBoard.kt
+++ src/commonMain/kotlin/entities/MarketBoard.kt
@@ -1,7 +1,10 @@
 package cloud.drakon.ktuniversalis.entities
 
+import kotlinx.serialization.Serializable
+
 @Deprecated(
     "Will be removed when union types are available within Kotlin.",
     level = DeprecationLevel.WARNING
 )
+@Serializable
 sealed interface MarketBoard
