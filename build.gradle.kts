6b05e6aa50a8e9905f11b9f38f8162ceee1f9825
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-10-09 15:00:50 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.9.0"
 
     id("org.jetbrains.kotlinx.kover") version "0.7.3"
-    id("org.sonarqube") version "4.3.1.3277"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 buildscript {
@@ -52,7 +52,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.4"
+        val ktorVersion = "2.3.5"
 
         val commonMain by getting {
             dependencies {
